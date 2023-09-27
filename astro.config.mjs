1ac06c7207e1d7260eba4dde6e1d2a3794ffb918
Tianzhou Chen <t@bytebase.com>
2023-09-28 00:56:03 +0800

    chore: minor tweak

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://www.mysql-vs-postgresql.com",
   integrations: [
     tailwind(),
     image({
