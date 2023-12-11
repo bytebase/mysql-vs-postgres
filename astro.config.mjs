86e91f3197c94ef730b2455d7fab030991e56b55
tianzhou <t@bytebase.com>
2023-12-11 16:44:15 +0800

    chore: update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.mysql-vs-postgresql.com",
+  site: "https://www.mysql-vs-postgres.com",
   integrations: [
     tailwind(),
     image({
